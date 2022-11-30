f82194f062b704f21cfd2492a568ca0bae31206e
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-11-30 19:58:11 +0000

    fetch

--- frontend/src/routes/home/home.component.jsx
+++ frontend/src/routes/home/home.component.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+
 const Home = () => {
-	return <div></div>;
+	const [posts, setPosts] = useState(null);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch('/api/posts');
+			const json = await response.json();
+
+			if (response.ok) {
+				setPosts(json);
+			}
+		};
+		fetchPosts();
+	}, []);
+
+	return (
+		<div>
+			{!!posts && (
+				<div>
+					{posts.map((post) => (
+						<p key={post._id}>{post.title}</p>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Home;
